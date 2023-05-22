e66b6e467773061b34d025b8425fc1f5090a3d50
KerlynLipa <kerlynlipaperez@gmail.com>
2023-05-22 01:17:48 -0500

    Añadido validadores para crear cliente y producto

--- farmacia/forms.py
+++ farmacia/forms.py
@@ -1,26 +1,77 @@
 from django import forms
 from .models import Cliente,Factura,DetalleFactura,Producto
-
-
+from django.core.validators import RegexValidator
+from django.core.validators import MinLengthValidator,validate_email,EmailValidator
+from django.core.exceptions import ValidationError
+import re
+#Añadir Widgets a todos para que validen los datos de entrada 
 
 class CreateClienteForm(forms.ModelForm):
+   
     class Meta:
         model = Cliente
         fields = ['nombre','direccion','telefono','email']
+        labels ={
+            'nombre': 'Nombre',
+            'direccion': 'Direccion',
+            'telefono': 'Telefono',
+            'email': 'Email'
+        }
+        widgets = {
+            'nombre': forms.TextInput(attrs={"class": "form-control"}),
+            'direccion': forms.TextInput(attrs={"class": "form-control"}),
+            'telefono': forms.NumberInput(attrs={"class": "form-control"})
+        }
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if nombre and not re.match(r'^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$', nombre):
+            raise ValidationError("El campo 'nombre' solo debe contener letras.")
+        return nombre
+
+    def clean_direccion(self):
+        direccion = self.cleaned_data.get('direccion')
+        if direccion and not re.match(r'^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\s]+$', direccion):
+            raise ValidationError("El campo 'direccion' solo debe contener letras y números.")
+        return direccion
+
 
 class CrearProductosForm(forms.ModelForm):
 
     
     class Meta:
         model = Producto
-        fields = ['nombre','descripcion','stock','precio_venta', 'precio_compra','stock']
-
-        widgets = {
-             'proveedor': forms.Select(attrs={"class": "form-control"}),
+        fields = ['nombre','descripcion','stock','precio_venta', 'precio_compra']
+        labels = {
+            'nombre':'Nombre',
+            'descripcion':'Descripcion',
+            'stock':'Stock',
+            'precio_venta':'Precio de venta',
+            'precio_compra':'Precio de compra'
         }
+        widgets = {
+            'nombre': forms.Select(attrs={"class": "form-control"}),
+            'descripcion': forms.Select(attrs={"class": "form-control","placeholder": "Escriba la descripción"}),
+
+        }
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if nombre and not re.match(r'^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$', nombre):
+            raise ValidationError("El campo 'nombre' solo debe contener letras.")
+        return nombre
+
+    def clean_descripcion(self):
+        descripcion = self.cleaned_data.get('descripcion')
+        if descripcion and not re.match(r'^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$', descripcion):
+            raise ValidationError("El campo 'descripcion' solo debe contener letras.")
+        return descripcion
+
+
 
 class CrearFacturasForm(forms.ModelForm):
     cliente = forms.ModelChoiceField(queryset=Cliente.objects.all())
+
+    
     class Meta:
         model = Factura  
         fields = ['cliente','total','descuento']
@@ -30,7 +81,8 @@ class CrearFacturasForm(forms.ModelForm):
             'descuento':'Descuento',
         }
         widgets = {
-             'cliente': forms.Select(attrs={"class": "form-control"}),
+            'cliente': forms.Select(attrs={"class": "form-control"}),
+
         }
    
 
@@ -41,5 +93,6 @@ class CrearDetalleFacturaForm(forms.ModelForm):
         model = DetalleFactura
         fields = ['producto','cantidad','precio_unitario','subtotal']
         widgets = {
+            
   
         }
